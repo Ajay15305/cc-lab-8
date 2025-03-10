8acfa706c14e72cf28cca1231e00fb4bfb2d54b0
Ajay15305 <143072193+Ajay15305@users.noreply.github.com>
2025-03-10 21:18:54 +0530

    Create newfile.cpp

---
+++ newfile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name = "User";
+    
+    std::cout << "Hello, " << name << "!" << std::endl;
+    std::cout << "Welcome to the this world!" << std::endl;
+    
+    return 0;
+}
